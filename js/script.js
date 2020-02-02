19ff82b4c10c76605c9f7eb9a3eca4382143cb33
Sascha Andeweg <sascha@designed.irish>
2020-02-02 13:25:07 +0000

    commit of the first steps in challenge 3

--- js/script.js
+++ js/script.js
@@ -4,4 +4,7 @@ $(document).ready(function(){
      var boxColor = $(this).css("background-color");
      $(".superButton").text(boxColor);
  })
+ $(".superButton").click(function(){
+     $(".superButton").text("Reset");
+ })
 });
