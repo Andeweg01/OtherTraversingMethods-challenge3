99ee5553a1ea927019eb70d037ce61a4a2b878c3
Sascha Andeweg <sascha@designed.irish>
2020-02-02 13:10:16 +0000

    jQuery script to change text in the superButton

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
  // put your code here
+ $("#panel").click(function(){
+     var boxColor = $(this).css("background-color");
+     $(".superButton").text(boxColor);
+ })
 });
