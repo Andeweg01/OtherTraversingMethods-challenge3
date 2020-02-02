7507eb681d0604592979b7b6aee3fa140d5bed89
Sascha Andeweg <sascha@designed.irish>
2020-02-02 13:35:27 +0000

    commit of final solution for challenge 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  // put your code here
- $("#panel").click(function(){
+ $(".theButton").click(function(){
      var boxColor = $(this).css("background-color");
      $(".superButton").text(boxColor);
  })
