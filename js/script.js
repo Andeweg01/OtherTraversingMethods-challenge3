48089a90e9e23959b3bbe86c28061d7f3061e21d
Sascha Andeweg <sascha@designed.irish>
2020-02-02 13:40:43 +0000

    commit to make sure the repository shows what what written

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
- // put your code here
+ // click to grab the rgb color of the clicked panel
  $(".theButton").click(function(){
      var boxColor = $(this).css("background-color");
      $(".superButton").text(boxColor);
  })
+ // click the reset button to return to initial text Reset
  $(".superButton").click(function(){
      $(".superButton").text("Reset");
  })
